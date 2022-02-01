1a33597ea028e4f9c656334aa7ccd422c4091f05
Sofia <lsofmascastanio@gmail.com>
2022-02-01 11:46:02 +0100

    relation user profil

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function profil(){
+        return $this->hasOne('App\Models\Profil');
+    }
 }
